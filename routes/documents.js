f463a23c736c050c86b486994186facc4d1d28a2
Patrick McElwee <pmcelwee@gmail.com>
2018-03-28 14:55:51 -0400

    Document route should properly report error to client
    Previously, it was returning the error message as if it was the body of
    the retrieved document. Now, it will return with the appropriate status
    code, plus a JSON error with details.

--- routes/documents.js
+++ routes/documents.js
@@ -4,6 +4,8 @@ const router = require('express').Router()
 const http = require('http')
 const options = require('../utils/options')()
 
+const processError = error => error.errorResponse
+
 router.get('/', (req, res) => {
   // getAuth(req).then(auth => {
   const uri = req.query.uri
@@ -20,22 +22,28 @@ router.get('/', (req, res) => {
   }
   const mlRequest = http.request(httpOptions, mlResponse => {
     let docBody = ''
-    mlResponse.on('data', (chunk) => {
+    mlResponse.on('data', chunk => {
       docBody += chunk
     })
     mlResponse.on('end', () => {
-      const contentType = mlResponse.headers['content-type']
-      if (contentType.includes('application/json')) {
-        docBody = JSON.parse(docBody)
+      if (mlResponse.statusCode === 200) {
+        const contentType = mlResponse.headers['content-type']
+        if (contentType.includes('application/json')) {
+          docBody = JSON.parse(docBody)
+        }
+        res.json({
+          content: docBody,
+          contentType: contentType
+        })
+      } else {
+        res
+          .status(mlResponse.statusCode)
+          .json(processError(JSON.parse(docBody)))
       }
-      res.json({
-        content: docBody,
-        contentType: contentType
-      })
     })
   })
 
-  mlRequest.on('error', (e) => {
+  mlRequest.on('error', e => {
     console.error(`problem with request: ${e.message}`)
   })
 
