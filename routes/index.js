06674c45acd4de7a724f05262166dd2324b6902e
Patrick McElwee <pmcelwee@gmail.com>
2018-04-17 15:12:18 -0400

    Add primitive ability to specify search result labels
    Passing in `extract` and `makeLabel()` to the route config. This is
    almost certainly not the final API, but needed to make it work for the
    moment, while we explore the right way.

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,26 @@ router.use(express.urlencoded({
 router.use(express.json())
 
 router.use('/auth', require('./auth'))
-router.use('/search', require('./search'))
+const authProvider = require('../utils/auth-helper')
+const searchRoute = require('./search')({
+  // authProvider
+  // - come up with an authProvider API
+  // can we replace this with middleware (before guarded routes, below unguarded routes)? This will stop it from ever reaching the guarded routes
+  // - still have to take into account that ML might not agree with middle-tier that you are authenticated (return a 401) - middle-tier should forward this along OR alert the Node app that authentication is invalid
+  // - also need the authenticator to authenticate each ML endpoint called
+  // authProvider.getAuthenticator
+  authProvider: authProvider,
+  // TODO: move this extract and makeLabel to a samplePerson branch
+  // because it is not generic, but it is useful for a quick MUIR demo
+  extract: '/name',
+  makeLabel: result => {
+    return result.extracted && result.extracted.content[0] && result.extracted.content[0].name
+  }
+  // mapping URIs to ids for CRUD in other routes
+  // even possibly going so far as to return IDs instead of URIs
+  // /dogs/1587 instead of dogs?uri=/somethingUgly
+})
+router.use('/search', searchRoute)
 router.use('/documents', require('./documents'))
 
 var four0four = require('../utils/404')()
--- routes/search.js
+++ routes/search.js
@@ -6,129 +6,159 @@ const http = require('http')
 const options = require('../utils/options')()
 const queryBuilder = require('marklogic').queryBuilder
 
-// TODO: DRY up into authHelper
-var authHelper = require('../utils/auth-helper')
-function getAuth(req) {
-  var user =
-    req.session.passport &&
-    req.session.passport.user &&
-    req.session.passport.user.username
-  return authHelper.getAuthorization(req.session, req.method, '/v1/search', {
-    authUser: user
-  })
-}
-
-// TODO: extract out to separate module that could alternatively
-// be run inside MarkLogic itself
-const processSearchResponse = function(searchResponse) {
-  const executionTime = parseFloat(
-    searchResponse.metrics['total-time'].replace(/^PT/, '')
-  )
-  const pageLength = searchResponse['page-length']
-  const page = Math.ceil(searchResponse.start / pageLength)
-  return {
-    query: {
-      queryText: searchResponse.qtext,
-      pageLength: pageLength,
-      page: page
-    },
-    response: {
-      metadata: {
-        executionTime: executionTime,
-        total: searchResponse.total
-      },
-      results: searchResponse.results,
-      facets: searchResponse.facets
-    }
-  }
-}
-
-// TODO: extract out to separate module that could alternatively
-// be run inside MarkLogic itself
-const buildMarklogicQuery = function(query) {
-  let structuredQuery
-  if (query.constraints) {
-    const constraintQueries = Object.keys(query.constraints).map(
-      constraintName => {
-        const andValues = query.constraints[constraintName].and || []
-        return queryBuilder.range(constraintName, andValues.map(c => c.name))
-      }
+module.exports = config => {
+  // TODO: rename to authProvider (?)
+  const authProvider = config.authProvider
+  if (!authProvider) {
+    throw new Error(
+      'muir-node-search configuration must include an authProvider'
     )
-    structuredQuery = {
-      queries: [
-        {
-          'and-query': {
-            queries: constraintQueries
-          }
-        }
-      ]
+  }
+  // TODO: DRY up into authProvider
+  function getAuth(req) {
+    var user =
+      req.session.passport &&
+      req.session.passport.user &&
+      req.session.passport.user.username
+    return authProvider.getAuthorization(req.session, req.method, '/v1/search', {
+      authUser: user
+    })
+  }
+
+  const processResults = results => {
+    results.forEach(result => {
+      if (config.makeLabel) {
+        result.label = config.makeLabel(result)
+      }
+    })
+    return results;
+  }
+
+  // TODO: extract out to separate module that could alternatively
+  // be run inside MarkLogic itself
+  const processSearchResponse = function(searchResponse) {
+    const executionTime = parseFloat(
+      searchResponse.metrics['total-time'].replace(/^PT/, '')
+    )
+    const pageLength = searchResponse['page-length']
+    const page = Math.ceil(searchResponse.start / pageLength)
+    return {
+      query: {
+        queryText: searchResponse.qtext,
+        pageLength: pageLength,
+        page: page
+      },
+      response: {
+        metadata: {
+          executionTime: executionTime,
+          total: searchResponse.total
+        },
+        results: processResults(searchResponse.results),
+        facets: searchResponse.facets
+      }
     }
   }
-  return JSON.stringify({
-    search: {
-      qtext: query.queryText,
-      query: structuredQuery
-    }
-  })
-}
 
-const processSearchError = error => error.errorResponse
-
-router.post('/', (req, res) => {
-  const query = req.body
-  const start = query.pageLength * (query.page - 1) + 1
-  getAuth(req).then(
-    auth => {
-      const httpOptions = {
-        // TODO: hanging with SSL at the moment
-        // protocol: options.httpsStrict ? 'https' : 'http',
-        hostname: options.mlHost,
-        port: options.mlHttpPort,
-        path:
-          '/v1/search?format=json&pageLength=' +
-          query.pageLength +
-          '&start=' +
-          start +
-          '&options=all', // TODO: make configurable and get from client
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-          authorization: auth
-        }
+  // TODO: extract out to separate module that could alternatively
+  // be run inside MarkLogic itself
+  const buildMarklogicQuery = function(query) {
+    let options = query.options || {};
+    if (config.extract) {
+      options["extract-document-data"] = {
+        "extract-path": config.extract
       }
-      const mlRequest = http.request(httpOptions, mlResponse => {
-        var mlSearchBody = ''
-        mlResponse.on('data', chunk => {
-          mlSearchBody += chunk
-        })
-        mlResponse.on('end', () => {
-          mlSearchBody = JSON.parse(mlSearchBody)
-          if (mlResponse.statusCode === 200) {
-            res.json(processSearchResponse(mlSearchBody))
-          } else {
-            res
-              .status(mlResponse.statusCode)
-              .json(processSearchError(mlSearchBody))
-          }
-        })
-      })
-
-      mlRequest.on('error', e => {
-        console.error(`problem with request: ${e.message}`)
-        res.status(500).json({ errorResponse: e })
-      })
-      const builtQuery = buildMarklogicQuery(query)
-      mlRequest.write(builtQuery)
-      mlRequest.end()
-    },
-    error => {
-      console.error('error authenticating search:', error);
-      res.status(401).json({
-        message: error
-      })
     }
-  )
-})
+    let structuredQuery
+    if (query.constraints) {
+      const constraintQueries = Object.keys(query.constraints).map(
+        constraintName => {
+          const andValues = query.constraints[constraintName].and || []
+          return queryBuilder.range(constraintName, andValues.map(c => c.name))
+        }
+      )
+      structuredQuery = {
+        queries: [
+          {
+            'and-query': {
+              queries: constraintQueries
+            }
+          }
+        ]
+      }
+    }
+    return JSON.stringify({
+      search: {
+        qtext: query.queryText,
+        query: structuredQuery,
+        options: options
+      }
+    })
+  }
 
-module.exports = router
+  const processSearchError = error => error.errorResponse
+
+  router.post('/', (req, res) => {
+    const query = req.body
+    const start = query.pageLength * (query.page - 1) + 1
+    getAuth(req).then(
+      auth => {
+        const httpOptions = {
+          // TODO: hanging with SSL at the moment
+          // protocol: options.httpsStrict ? 'https' : 'http',
+          hostname: options.mlHost,
+          port: options.mlHttpPort,
+          path:
+            '/v1/search?format=json&pageLength=' +
+            query.pageLength +
+            '&start=' +
+            start +
+            '&options=all', // TODO: make configurable and get from client
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            authorization: auth
+          }
+        }
+        const mlRequest = http.request(httpOptions, mlResponse => {
+          var mlSearchBody = ''
+          mlResponse.on('data', chunk => {
+            mlSearchBody += chunk
+          })
+          mlResponse.on('end', () => {
+            mlSearchBody = JSON.parse(mlSearchBody)
+            if (mlResponse.statusCode === 200) {
+              res.json(processSearchResponse(mlSearchBody))
+            } else {
+              res
+                .status(mlResponse.statusCode)
+                .json(processSearchError(mlSearchBody))
+            }
+          })
+        })
+
+        mlRequest.on('error', e => {
+          console.error(`problem with request: ${e.message}`)
+          res.status(500).json({ errorResponse: e })
+        })
+        const builtQuery = buildMarklogicQuery(query)
+        mlRequest.write(builtQuery)
+        mlRequest.end()
+      },
+      error => {
+        console.error('error authenticating search:', error)
+        res.status(401).json({
+          message: error
+        })
+      }
+    ).catch(error => {
+      // TODO: DRY up errors and make it standard across plugins
+      console.error(error)
+      res.status(500).json({
+        message: error.message
+      })
+    })
+  })
+
+  return router
+}
