f52bf25ec50623a45e807116dc0c133b8850971c
Patrick McElwee <pmcelwee@gmail.com>
2018-05-02 10:51:26 -0400

    Refactor utils/options
    Make it more readable

--- utils/options.js
+++ utils/options.js
@@ -1,80 +1,75 @@
 'use strict'
 
 var options
-module.exports = function() {
-  // memoize the options: shouldn't change during a single run of the server
-  if (options) {
-    return options
+var optionsNotSetByUser = []
+var availableOptions = {
+  env: {
+    default: 'development',
+    variable: 'NODE_ENV'
+  },
+  appName: {
+    default: 'muir-app',
+    variable: 'MUIR_APP_NAME'
+  },
+  sessionSecret: {
+    default: 'D5sktFU2flpH&fPzf6Sw',
+    variable: 'MUIR_SESSION_SECRET'
+  },
+  appPort: {
+    default: 9003,
+    variable: 'MUIR_APP_PORT',
+    coerce: 'port'
+  },
+  mlHost: {
+    default: 'localhost',
+    variable: 'MUIR_ML_HOST'
+  },
+  mlRestPort: {
+    default: 8063,
+    variable: 'MUIR_ML_REST_PORT',
+    coerce: 'port'
+  },
+  disallowUpdates: {
+    default: false,
+    variable: 'MUIR_DISALLOW_UPDATES',
+    coerce: 'boolean'
+  },
+  appUsersOnly: {
+    default: false,
+    variable: 'MUIR_APP_USERS_ONLY',
+    coerce: 'boolean'
   }
+}
 
-  var availableOptions = {
-    env: {
-      default: 'development',
-      variable: 'NODE_ENV'
-    },
-    appName: {
-      default: 'muir-app',
-      variable: 'MUIR_APP_NAME'
-    },
-    sessionSecret: {
-      default: 'D5sktFU2flpH&fPzf6Sw',
-      variable: 'MUIR_SESSION_SECRET'
-    },
-    appPort: {
-      default: 9003,
-      variable: 'MUIR_APP_PORT',
-      coerce: 'port'
-    },
-    mlHost: {
-      default: 'localhost',
-      variable: 'MUIR_ML_HOST'
-    },
-    mlRestPort: {
-      default: 8063,
-      variable: 'MUIR_ML_REST_PORT',
-      coerce: 'port'
-    },
-    disallowUpdates: {
-      default: false,
-      variable: 'MUIR_DISALLOW_UPDATES',
-      coerce: 'boolean'
-    },
-    appUsersOnly: {
-      default: false,
-      variable: 'MUIR_APP_USERS_ONLY',
-      coerce: 'boolean'
-    }
+function coerceToPort(port) {
+  var coerced = parseInt(port)
+  if (coerced < 1 || coerced > 65535) {
+    console.error(
+      '\nYou specified an invalid port: ' +
+        coerced +
+        '. Please check your application configuration.'
+    )
+    return
   }
+  return coerced
+}
 
-  function coerceToPort(port) {
-    var coerced = parseInt(port)
-    if (coerced < 1 || coerced > 65535) {
-      console.error(
-        '\nYou specified an invalid port: ' +
-          coerced +
-          '. Please check your application configuration.'
-      )
-      return
-    }
-    return coerced
+function coerceToBool(x) {
+  return x === 'true' || x === true
+}
+
+function coerce(value, coerceTo) {
+  switch (coerceTo) {
+    case 'boolean':
+      return coerceToBool(value)
+    case 'port':
+      return coerceToPort(value)
+    default:
+      return value
   }
+}
 
-  function coerceToBool(x) {
-    return x === 'true' || x === true
-  }
-
-  function coerce(value, coerceTo) {
-    switch (coerceTo) {
-      case 'boolean':
-        return coerceToBool(value)
-      case 'port':
-        return coerceToPort(value)
-      default:
-        return value
-    }
-  }
-
-  var optionsNotSetByUser = []
+function setOptions() {
   options = {}
   Object.keys(availableOptions).forEach(function(optionKey) {
     var optionConfig = availableOptions[optionKey]
@@ -86,7 +81,9 @@ module.exports = function() {
       options[optionKey] = coerce(providedValue, optionConfig.coerce)
     }
   })
+}
 
+function warnIfOptionsNotSpecified() {
   if (optionsNotSetByUser.length > 0) {
     console.warn(
       '\nYou did not specify the following environment variables for app configuration: ' +
@@ -94,12 +91,19 @@ module.exports = function() {
         '. This might be fine, if you are comfortable with the default settings. Consult the README on how to configure this application.\n'
     )
   }
+}
+
+module.exports = function() {
+  // memoize the options: shouldn't change during a single run of the server
+  if (!options) {
+    setOptions()
+    warnIfOptionsNotSpecified()
+  }
+  return options
   // if (options.httpsStrict) {
   //   console.info('Self signed certificates not allowed.')
   // } else {
   //   console.warn('Allowing self signed certificates. Not advisable on production.')
   //   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
   // }
-
-  return options
 }
