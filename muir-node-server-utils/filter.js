508eb18e9a70322db55b5da2dec95bc5ebad318b
Geert Josten <geert.josten@marklogic.com>
2018-06-20 07:50:39 +0200

    Using new querybuilder to finish filter lib

--- muir-node-server-utils/filter.js
+++ muir-node-server-utils/filter.js
@@ -1,20 +1,58 @@
 
-const queryBuilder = require('marklogic').queryBuilder
+//const queryBuilder = require('marklogic').queryBuilder
+const queryBuilder = require('./ml-query-builder.service.js')({});
 
 var filter = (function() {
   var queryTextDefaultHandler = function(filter) {
-    // TODO: how to support constraint?
-    return queryBuilder.parsedFrom(filter.value);
+    var q;
+    if (filter.constraint) {
+      // note: the value will not be 'parsed' in this case, unless the constraint takes care of it
+      q = constraint(filter.constraintType, filter.constraint, 'EQ', filter.value);
+    } else {
+      q = { qtext: filter.value };
+    }
+    return q;
   };
 
   var selectionDefaultHandler = function(filter) {
-    // TODO: find a way to build constraint queries!
-    return null; //queryBuilder.parsedFrom(filter.value);
+    var arr = filter.value;
+    if (!Array.isArray(arr)) {
+      arr = [arr];
+    }
+    var queries = arr.map(function(item) {
+      if (item.not) {
+        // negated
+        return constraint(filter.constraintType, filter.constraint, 'NE', item.not);
+      } else {
+        // atomic value
+        return constraint(filter.constraintType, filter.constraint, 'EQ', item);
+      }
+    });
+    if (queries.length === 1) {
+      return queries[0]
+    } else {
+      if (filter.mode === 'or') {
+        return queryBuilder.or(queries);
+      } else {
+        return queryBuilder.and(queries);
+      }
+    }
   };
 
   var rangeDefaultHandler = function(filter) {
-    // TODO: find a way to build constraint queries!
-    return null; //queryBuilder.parsedFrom(filter.value);
+    var queries = Object.keys(filter.value).map(function(key) {
+      // TODO: iterate values in case of and-mode
+      return constraint(filter.constraintType, filter.constraint, key.toUpperCase(), filter.value[key]);
+    });
+    if (queries.length === 1) {
+      return queries[0]
+    } else {
+      if (filter.mode === 'or') {
+        return queryBuilder.or(queries);
+      } else {
+        return queryBuilder.and(queries);
+      }
+    }
   };
 
   var registerFilterType = function(type, handler) {
@@ -59,7 +97,7 @@ var filter = (function() {
     } else {
       var handler = handlers[filters.type || 'selection'];
       if (handler) {
-        handler(filters);
+        return handler(filters);
       } else {
         throw new Error("No handler found for filter: " + JSON.stringify(filters));
       }
@@ -70,6 +108,23 @@ var filter = (function() {
     registerFilterType: registerFilterType,
     buildQuery: buildQuery
   };
+
+  function constraint(type, name, operator, value) {
+    var c = queryBuilder.ext.constraint(type || 'range');
+    if (type === 'range') {
+      return c(name, operator, value);
+    } else if (type === 'custom') {
+      return c(name, { operator: operator, value: value });
+    } else {
+      return c(name, value);
+    }
+  }
+
+  // copied from Angular.js
+  function isObject(value) {
+    // http://jsperf.com/isobject4
+    return value !== null && typeof value === 'object';
+  }
 })();
 
 module.exports = filter;
