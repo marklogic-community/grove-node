0350ed56455e47a00d472153bc261bd1545acb07
Geert Josten <geert.josten@marklogic.com>
2018-06-20 07:51:52 +0200

    Using backend lib for search middle-ware

--- muir-default-routes/search.js
+++ muir-default-routes/search.js
@@ -7,6 +7,7 @@ var provider = (function() {
   // const https = require('https')
   const options = require('../muir-node-server-utils/options')()
   // const queryBuilder = require('marklogic').queryBuilder
+  const backend = require('../muir-node-server-utils/backend')
 
   var provide = function(config) {
 
@@ -46,27 +47,21 @@ var provider = (function() {
     // be run inside MarkLogic itself
     const buildMarklogicQuery = function(query) {
       let options = query.options || {};
+
       if (config.extract) {
         options["extract-document-data"] = {
           "extract-path": config.extract
         }
       }
-      // TODO: cleanup..
-      var appendText = '';
-      if (query.constraints) {
-        Object.keys(query.constraints).forEach(key => {
-          query.constraints[key].and.forEach(value => {
-            appendText = appendText + ' ' + key + ':' + '"' + value.value + '"'
-          })
-        })
-      }
+
       var structuredQuery = {}
       if (query.filters) {
         structuredQuery = require('../muir-node-server-utils/filter').buildQuery(query.filters)
       }
+
       return JSON.stringify({
         search: {
-          qtext: (query.queryText || '') + appendText,
+          qtext: (query.queryText || ''),
           query: structuredQuery,
           options: options
         }
@@ -83,55 +78,40 @@ var provider = (function() {
 
     router.post('/', (req, res) => {
       const query = req.body
-      const httpOptions = {
-        // TODO: hanging with SSL at the moment
-        // protocol: options.httpsStrict ? 'https' : 'http',
-        hostname: options.mlHost,
-        port: options.mlRestPort,
-        path:
-          '/v1/search?format=json&pageLength=' +
-          query.options.pageLength +
-          '&start=' +
-          query.options.start +
-          '&options=' +
-          (config.namedOptions || 'all'),
+      const reqOptions = {
         method: 'POST',
+        path: '/v1/search',
+        params: {
+          format: 'json',
+          start: query.options.start,
+          pageLength: query.options.pageLength,
+          options: (config.namedOptions || 'all')
+        },
         headers: {
           'Content-Type': 'application/json',
           Accept: 'application/json'
         }
       }
-      authProvider.getAuth(req.session, httpOptions).then(
+      authProvider.getAuth(req.session, reqOptions).then(
         auth => {
           if (auth) {
-            httpOptions.headers.Authorization = auth
+            reqOptions.headers.Authorization = auth
           }
 
-          // TODO: make use of backend client here?
-          const mlRequest = http.request(httpOptions, mlResponse => {
-            var mlSearchBody = ''
-            mlResponse.on('data', chunk => {
-              mlSearchBody += chunk
-            })
-            mlResponse.on('end', () => {
-              mlSearchBody = JSON.parse(mlSearchBody)
-              if (mlResponse.statusCode === 200) {
-                res.json(processSearchResponse(mlSearchBody))
-              } else {
-                res
-                  .status(mlResponse.statusCode)
-                  .json(processSearchError(mlSearchBody))
-              }
-            })
-          })
-
-          mlRequest.on('error', e => {
-            console.error(`problem with request: ${e.message}`)
-            res.status(500).json({ errorResponse: e })
-          })
           const builtQuery = buildMarklogicQuery(query)
-          mlRequest.write(builtQuery)
-          mlRequest.end()
+          console.log(builtQuery);
+          reqOptions.body = builtQuery;
+
+          backend.call(req, reqOptions, function(backendResponse, data) {
+            var json = JSON.parse(data.toString());
+            if (backendResponse.statusCode === 200) {
+              res.json(processSearchResponse(json))
+            } else {
+              res
+              .status(backendResponse.statusCode)
+              .json(processSearchError(json))
+            }
+          })
         },
         error => {
           console.error('error authenticating search:', error)
