b940ed8b6c092231872d8d4ba317c2601929f21f
Patrick McElwee <pmcelwee@gmail.com>
2018-09-26 13:50:12 -0400

    GROVE-318: Check if build directory exists before serving statics
    To avoid confusing 404s that leak information about the path to the
    application on the server.

--- grove-default-routes/static.js
+++ grove-default-routes/static.js
@@ -3,14 +3,25 @@
 const provider = (function() {
   const express = require('express');
   const path = require('path');
+  const fs = require('fs');
   const options = require('../grove-node-server-utils/options')();
 
   const provide = function() {
     const router = express.Router();
 
     const staticUIPath = path.resolve(options.staticUIDirectory);
-    router.use(express.static(staticUIPath));
-    router.get('/*', (req, res) => res.sendFile(staticUIPath + '/index.html'));
+    if (fs.existsSync(staticUIPath + '/index.html')) {
+      router.use(express.static(staticUIPath));
+      router.get('/*', (req, res) =>
+        res.sendFile(staticUIPath + '/index.html')
+      );
+    } else {
+      console.warn(
+        'There is no index.html located at the GROVE_UI_BUILD_PATH: ' +
+          staticUIPath +
+          '. This middle-tier will not try to serve static UI assets.'
+      );
+    }
 
     return router;
   };
