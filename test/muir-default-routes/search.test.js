360793ef0a0a38fc7be0d919f854995aa827d5c3
Patrick McElwee <pmcelwee@gmail.com>
2018-11-30 15:30:24 -0500

    Add a custom constraint search test
    That already works, thankfully. :-)

--- test/muir-default-routes/search.test.js
+++ test/muir-default-routes/search.test.js
@@ -49,6 +49,8 @@ describe('defaultSearchRoute', () => {
     });
 
     it('correctly parses a geospatial constraint filter', done => {
+      // TODO: add unit tests for the parsing of different kinds of geospatial
+      // queries
       const search = searchProvider({
         authProvider: minAuthProvider
       });
@@ -88,6 +90,74 @@ describe('defaultSearchRoute', () => {
             constraint: 'Location',
             mode: 'and',
             value: [myBox]
+          }
+        })
+        .then(response => {
+          expect(response).to.have.status(200);
+          done();
+        });
+    });
+
+    it('correctly parses custom constraint with 2 values', done => {
+      const search = searchProvider({
+        authProvider: minAuthProvider
+      });
+      const myBox1 = {
+        north: 100,
+        south: 0,
+        west: 150,
+        east: -150
+      };
+      const myBox2 = {
+        north: 150,
+        south: 50,
+        west: 125,
+        east: -100
+      };
+      const myBoxes = [myBox1, myBox2];
+      // TODO: if we allow execution to be configurable with a func, we could
+      // eliminate the network mocking ('nock'-ing)
+      nock(marklogicURL)
+        .post('/v1/search', {
+          search: {
+            query: {
+              'or-query': {
+                queries: [
+                  {
+                    'custom-constraint-query': {
+                      'constraint-name': 'CustomLocation',
+                      value: myBox1,
+                      operator: 'EQ'
+                    }
+                  },
+                  {
+                    'custom-constraint-query': {
+                      'constraint-name': 'CustomLocation',
+                      value: myBox2,
+                      operator: 'EQ'
+                    }
+                  }
+                ]
+              }
+            },
+            options: {}
+          }
+        })
+        .query(() => true)
+        .reply(200, {});
+      app.use(search);
+      chai
+        .request(app)
+        .post('/')
+        .send({
+          filters: {
+            // not sure if this type makes sense
+            type: 'selection',
+            constraintType: 'custom',
+            constraint: 'CustomLocation',
+            // is this the right mode?
+            mode: 'or',
+            value: myBoxes
           }
         })
         .then(response => {
