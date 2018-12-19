fb7682c6ac5d2c2624db7053fb8f4f5272c29151
Patrick McElwee <pmcelwee@gmail.com>
2018-12-19 12:17:05 -0500

    GROVE-489: add basic geospatial filter 'and' and 'or' tests

--- test/grove-node-server-utils/filter.test.js
+++ test/grove-node-server-utils/filter.test.js
@@ -10,4 +10,136 @@ describe('filter.js', () => {
       expect(buildQuery({})).to.deep.equal({});
     });
   });
+
+  it('works for various geospatial types, anded together', () => {
+    const box = {
+      north: 100,
+      south: 0,
+      west: 150,
+      east: -150
+    };
+    const point = { latitude: 0, longitude: 100 };
+    const circle = {
+      radius: 100,
+      point: { latitude: 100, longitude: 0 }
+    };
+    const polygon = {
+      point: [{ latitude: 0, longitude: 100 }, { latitude: 100, longitude: 0 }]
+    };
+    const filter = {
+      type: 'selection',
+      constraintType: 'geospatial',
+      constraint: 'Location',
+      mode: 'and',
+      value: [box, point, circle, polygon]
+    };
+    expect(buildQuery(filter)).to.deep.equal({
+      'and-query': {
+        queries: [
+          {
+            'geospatial-constraint-query': {
+              'constraint-name': 'Location',
+              box: [box],
+              point: [],
+              circle: [],
+              polygon: []
+            }
+          },
+          {
+            'geospatial-constraint-query': {
+              'constraint-name': 'Location',
+              box: [],
+              point: [point],
+              circle: [],
+              polygon: []
+            }
+          },
+          {
+            'geospatial-constraint-query': {
+              'constraint-name': 'Location',
+              box: [],
+              point: [],
+              circle: [circle],
+              polygon: []
+            }
+          },
+          {
+            'geospatial-constraint-query': {
+              'constraint-name': 'Location',
+              box: [],
+              point: [],
+              circle: [],
+              polygon: [polygon]
+            }
+          }
+        ]
+      }
+    });
+  });
+
+  it('works for various geospatial types, "or"ed together', () => {
+    const box = {
+      north: 100,
+      south: 0,
+      west: 150,
+      east: -150
+    };
+    const point = { latitude: 0, longitude: 100 };
+    const circle = {
+      radius: 100,
+      point: { latitude: 100, longitude: 0 }
+    };
+    const polygon = {
+      point: [{ latitude: 0, longitude: 100 }, { latitude: 100, longitude: 0 }]
+    };
+    const filter = {
+      type: 'selection',
+      constraintType: 'geospatial',
+      constraint: 'Location',
+      mode: 'or',
+      value: [box, point, circle, polygon]
+    };
+    expect(buildQuery(filter)).to.deep.equal({
+      'or-query': {
+        queries: [
+          {
+            'geospatial-constraint-query': {
+              'constraint-name': 'Location',
+              box: [box],
+              point: [],
+              circle: [],
+              polygon: []
+            }
+          },
+          {
+            'geospatial-constraint-query': {
+              'constraint-name': 'Location',
+              box: [],
+              point: [point],
+              circle: [],
+              polygon: []
+            }
+          },
+          {
+            'geospatial-constraint-query': {
+              'constraint-name': 'Location',
+              box: [],
+              point: [],
+              circle: [circle],
+              polygon: []
+            }
+          },
+          {
+            'geospatial-constraint-query': {
+              'constraint-name': 'Location',
+              box: [],
+              point: [],
+              circle: [],
+              polygon: [polygon]
+            }
+          }
+        ]
+      }
+    });
+  });
 });
