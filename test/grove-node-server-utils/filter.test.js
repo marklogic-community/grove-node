45ef751a7f5a60bd5b3578d64d5a6996685933e6
Patrick McElwee <pmcelwee@gmail.com>
2018-12-20 14:35:25 -0500

    GROVE-489: add custom-constraint-query test for filters

--- test/grove-node-server-utils/filter.test.js
+++ test/grove-node-server-utils/filter.test.js
@@ -11,6 +11,135 @@ describe('filter.js', () => {
     });
   });
 
+  describe('selection', () => {
+    it('works with simple example', () => {
+      // TODO
+    });
+
+    describe('geospatial', () => {
+      const box = {
+        north: 100,
+        south: 0,
+        west: 150,
+        east: -150
+      };
+      const point = { latitude: 0, longitude: 100 };
+      const circle = {
+        radius: 100,
+        point: { latitude: 100, longitude: 0 }
+      };
+      const polygon = {
+        point: [
+          { latitude: 0, longitude: 100 },
+          { latitude: 100, longitude: 0 }
+        ]
+      };
+      const filter = {
+        type: 'selection',
+        constraintType: 'geospatial',
+        constraint: 'Location',
+        mode: 'and',
+        value: [box, point, circle, polygon]
+      };
+      const expectedQueries = {
+        queries: [
+          {
+            'geospatial-constraint-query': {
+              'constraint-name': 'Location',
+              box: [box],
+              point: [],
+              circle: [],
+              polygon: []
+            }
+          },
+          {
+            'geospatial-constraint-query': {
+              'constraint-name': 'Location',
+              box: [],
+              point: [point],
+              circle: [],
+              polygon: []
+            }
+          },
+          {
+            'geospatial-constraint-query': {
+              'constraint-name': 'Location',
+              box: [],
+              point: [],
+              circle: [circle],
+              polygon: []
+            }
+          },
+          {
+            'geospatial-constraint-query': {
+              'constraint-name': 'Location',
+              box: [],
+              point: [],
+              circle: [],
+              polygon: [polygon]
+            }
+          }
+        ]
+      };
+      it('works for various geospatial types, anded together', () => {
+        expect(buildQuery(filter)).to.deep.equal({
+          'and-query': expectedQueries
+        });
+      });
+
+      it('works for various geospatial types, "or"ed together', () => {
+        filter.mode = 'or';
+        expect(buildQuery(filter)).to.deep.equal({
+          'or-query': expectedQueries
+        });
+      });
+    });
+
+    describe('custom constraint', () => {
+      const myValues = [{ value: 1 }, { value: 2 }];
+      const filter = {
+        // not sure if this type makes sense
+        type: 'selection',
+        constraintType: 'custom',
+        constraint: 'CustomConstraint',
+        // is this the right mode?
+        mode: 'and',
+        value: myValues
+      };
+      const expectedQueries = {
+        queries: [
+          {
+            'custom-constraint-query': {
+              'constraint-name': 'CustomConstraint',
+              value: { value: 1 },
+              operator: 'EQ'
+            }
+          },
+          {
+            'custom-constraint-query': {
+              'constraint-name': 'CustomConstraint',
+              value: { value: 2 },
+              operator: 'EQ'
+            }
+          }
+        ]
+      };
+
+      it('works in and mode', () => {
+        expect(buildQuery(filter)).to.deep.equal({
+          'and-query': expectedQueries
+        });
+      });
+
+      it('works in or mode', () => {
+        filter.mode = 'or';
+        expect(buildQuery(filter)).to.deep.equal({
+          'or-query': expectedQueries
+        });
+      });
+    });
+  });
+
   describe('range', () => {
     it('works with simple example', () => {
       expect(
@@ -43,82 +172,6 @@ describe('filter.js', () => {
             }
           ]
         }
-      });
-    });
-  });
-
-  describe('geospatial', () => {
-    const box = {
-      north: 100,
-      south: 0,
-      west: 150,
-      east: -150
-    };
-    const point = { latitude: 0, longitude: 100 };
-    const circle = {
-      radius: 100,
-      point: { latitude: 100, longitude: 0 }
-    };
-    const polygon = {
-      point: [{ latitude: 0, longitude: 100 }, { latitude: 100, longitude: 0 }]
-    };
-    const filter = {
-      type: 'selection',
-      constraintType: 'geospatial',
-      constraint: 'Location',
-      mode: 'and',
-      value: [box, point, circle, polygon]
-    };
-    const expectedQueries = {
-      queries: [
-        {
-          'geospatial-constraint-query': {
-            'constraint-name': 'Location',
-            box: [box],
-            point: [],
-            circle: [],
-            polygon: []
-          }
-        },
-        {
-          'geospatial-constraint-query': {
-            'constraint-name': 'Location',
-            box: [],
-            point: [point],
-            circle: [],
-            polygon: []
-          }
-        },
-        {
-          'geospatial-constraint-query': {
-            'constraint-name': 'Location',
-            box: [],
-            point: [],
-            circle: [circle],
-            polygon: []
-          }
-        },
-        {
-          'geospatial-constraint-query': {
-            'constraint-name': 'Location',
-            box: [],
-            point: [],
-            circle: [],
-            polygon: [polygon]
-          }
-        }
-      ]
-    };
-    it('works for various geospatial types, anded together', () => {
-      expect(buildQuery(filter)).to.deep.equal({
-        'and-query': expectedQueries
-      });
-    });
-
-    it('works for various geospatial types, "or"ed together', () => {
-      filter.mode = 'or';
-      expect(buildQuery(filter)).to.deep.equal({
-        'or-query': expectedQueries
       });
     });
   });
